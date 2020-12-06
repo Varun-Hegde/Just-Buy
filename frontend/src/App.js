80d64ab8c6dfd025d78e2a4af036b2805a81e0e7
Varun Hegde <varunhegde2k@gmail.com>
2020-12-07 02:00:02 +0530

    Added Header,Footer and HomeScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import './bootstrap.min.css'
+import {Container} from 'react-bootstrap'
+import './index.css'
+import HomeScreen from './screens/HomeScreen'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+        <main className='py-3'>
+          <Container>
+            <HomeScreen />
+          </Container>
+        </main>
+      <Footer />
+    </>
   );
 }
 
