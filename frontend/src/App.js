74370926943fcb3eda4a3da0cc98b98f62eb324b
Varun Hegde <varunhegde2k@gmail.com>
2020-12-07 02:24:49 +0530

    Added react-router-dom ProductScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,25 @@ import Footer from './components/Footer'
 import './bootstrap.min.css'
 import {Container} from 'react-bootstrap'
 import './index.css'
-import HomeScreen from './screens/HomeScreen'
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+ 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
         <main className='py-3'>
           <Container>
-            <HomeScreen />
+            <Switch>
+              <Route path='/' component={HomeScreen}  exact />
+              <Route path='/product/:id' component={ProductScreen}  />
+            </Switch>
           </Container>
         </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,7 @@ import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import products from '../products'
 
+
 const HomeScreen = () => {
   return (
     <>
