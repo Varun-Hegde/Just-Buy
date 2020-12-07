aa67fa94ee94a9aa1251aa1fd8d58d6f0fc0024b
Varun Hegde <varunhegde2k@gmail.com>
2020-12-08 00:11:22 +0530

    Basic Routes and errorHandler, pageNotFound middlewear

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,44 @@
 const express = require('express')
-const products = require('./data/products')
 const dotenv = require('dotenv')
+
 //GET THE CONNECTION TO MONGO
 const connectDB = require('./config/db')
 
 //LOGGER
 const morgan = require('morgan')
 
+//IMPORT ROUTES 
+const productRoutes = require('./routes/productRoutes')
+
+//IMPORT ERROR HANDLER AND PAGE NOT FOUND 
+const {notFound,errorHandler} = require('./middlewear/errorMiddlewear')
+
 dotenv.config()
  
 const app = express()
+
+//CONNECT TO DATABASE
 connectDB()
 
+//MIDDLEWEARS
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 
-
+//ROUTES
 app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+//PRODUCT ROUTE
+app.use('/api/products',productRoutes)
+ 
+//PAGE NOT FOUND
+app.use(notFound)
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+//ERROR HANDLER
+app.use(errorHandler)
 
-
+//LISTEN 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
