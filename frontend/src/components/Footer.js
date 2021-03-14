a8a191c01eedce7035d80b36506cf2f0f4963b6f
Varun Hegde <varunhegde2k@gmail.com>
2021-03-15 00:08:38 +0530

    Added footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import {Container,Row,Col} from 'react-bootstrap'
 
+
+
 const Footer = () => {
     return (
         <footer>
             <Container>
-                <Row>
+                <Row className='d-flex flex-column'>
                     <Col className='text-center py-3'>
-                        Copyright &copy; eKart
+                        Copyright &copy; TrekNTravel
+                    </Col>
+                    <Col className='text-center pb-3'>
+                        <a style={{textDecoration:'none'}} target="_blank" href='https://github.com/Varun-Hegde'>
+                                Built with <i style={{color:"red"}} className="fas fa-heart"></i> by Varun
+                        </a>
                     </Col>
                 </Row>
+                
             </Container>
         </footer>
     )
